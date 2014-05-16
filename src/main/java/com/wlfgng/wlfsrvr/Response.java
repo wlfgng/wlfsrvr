6d94955eb1d981d3e0563bccf43efaa2bb5e41c1
Paul Howden <phowden@oswego.edu>
2014-05-16 11:16:19 -0400

    Added response type error and message field

--- src/main/java/com/wlfgng/wlfsrvr/Response.java
+++ src/main/java/com/wlfgng/wlfsrvr/Response.java
@@ -6,15 +6,27 @@ public class Response implements Serializable{
 
 	private RespType type;
 	private HashMap<String,String> results;
+	private String message;
 
 	public Response(RespType t){
 		this.type = t;
-		results = null;
+		this.message = null;
+		this.results = null;
+	}
+
+	public Response(RespType t, String m){
+		this.type = t;
+		this.message = m;
+		this.results = null;
 	}
 
 	public Response(RespType t, HashMap<String,String> r){
 		this.type = t;
 		this.results = r;
+	}
+
+	public void setMessage(String msg){
+		this.message = msg;
 	}
 
 	public HashMap<String,String> getResults(){
@@ -23,6 +35,10 @@ public class Response implements Serializable{
 
 	public RespType getType(){
 		return type;
+	}
+
+	public String getMessage(){
+		return message;
 	}
 
 }
